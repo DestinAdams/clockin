c4140e41e576a407a5f46889e35a62bd74fc7da0
Worryinghippo2 <dadams34@asu.edu>
2025-05-27 13:26:56 -0700

    v2

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-export default function dashboard() {
+
+
+export default function Dashboard() {
     return (
-        <div><h1 className="text-black">dashboard</h1></div>
+        <p>dashboard page</p>
     );
 }
