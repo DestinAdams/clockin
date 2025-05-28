a6c2d596c4592f9e1bc82727a19c5b5c9b4a48da
Worryinghippo2 <dadams34@asu.edu>
2025-05-28 00:44:21 -0700

    postgres init

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,10 @@
+// import getTestTableRows from '@/app/lib/data';
 import React from "react";
 
-
 export default function Dashboard() {
+    // const rows = await getTestTableRows();
+
     return (
-        <p>Dashddwdrd page</p>
+        <p>dadad</p>
     );
 }
