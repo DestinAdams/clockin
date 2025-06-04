244711eda972fe37d466f665ec94075f8c6385aa
Worryinghippo2 <dadams34@asu.edu>
2025-06-04 15:34:32 -0700

    Update SideNav.tsx

--- src/app/ui/sidenav.tsx
+++ src/app/ui/sidenav.tsx
@@ -67,14 +67,14 @@ function SideNav() {
 
                 {/* Bottom section */}
                 <div className="p-4">
-                    <Link
-                        href="/"
-                        className="flex items-center gap-2 p-1 rounded hover:bg-gray-100 border text-gray-700 hover:text-blue-600"
-                    >
-                        <div><House size={20} color='black' /></div>
+                        <Link
+                            href="/"
+                            className="flex items-center gap-2 p-1 rounded hover:bg-gray-100 border text-gray-700 hover:text-blue-600"
+                        >
+                            <div><House size={20} color='black' /></div>
 
-                        {!isCollapsed && <span>Home</span>}
-                    </Link>
+                            {!isCollapsed && <span>Home</span>}
+                        </Link>
                 </div>
             </nav>
         </div>
