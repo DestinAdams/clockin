b3517e5d1485892791f961dbe6c83640de430d8a
Worryinghippo2 <dadams34@asu.edu>
2025-06-30 21:46:16 -0700

    update eslintconfig

--- src/app/components/editEntryButton.tsx
+++ src/app/components/editEntryButton.tsx
@@ -1,21 +1,21 @@
-'use client';
+// 'use client';
 
-import { Edit } from "lucide-react";
+// import { Edit } from "lucide-react";
 
-interface EditEntryButtonProps {
-    entry_id: number;
-}
+// interface EditEntryButtonProps {
+//     entry_id: number;
+// }
 
-export default function EditEntryButton({ entry_id }: EditEntryButtonProps) {
-    return (
-        <button
-            onClick={() => {
-                alert('hey you clicked edit! implement me!');
-            }}
-        // disabled={isDeleting}
-        // className="disabled:opacity-25"
-        >
-            <Edit className="text-gray-600 hover:text-gray-700" />
-        </button>
-    );
-}
+// export default function EditEntryButton({ entry_id }: EditEntryButtonProps) {
+//     return (
+//         <button
+//             onClick={() => {
+//                 alert('hey you clicked edit! implement me!');
+//             }}
+//         // disabled={isDeleting}
+//         // className="disabled:opacity-25"
+//         >
+//             <Edit className="text-gray-600 hover:text-gray-700" />
+//         </button>
+//     );
+// }
