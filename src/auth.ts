9e734abd9c4cf4ba5df2a58ddb05d1cdda90adc3
Worryinghippo2 <dadams34@asu.edu>
2025-07-23 00:35:42 -0700

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,26 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import NeonAdapter from "@auth/neon-adapter";
+import { Pool } from "@neondatabase/serverless";
 
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
-})
+export const { handlers, auth, signIn, signOut } = NextAuth(() => {
+  const { DATABASE_URL, AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET } = process.env;
+
+  if (!DATABASE_URL || !AUTH_GOOGLE_ID || !AUTH_GOOGLE_SECRET) {
+    throw new Error("Missing required env vars");
+  }
+
+  const pool = new Pool({ connectionString: DATABASE_URL });
+
+  return {
+    adapter: NeonAdapter(pool),
+    providers: [
+      Google({
+        clientId: AUTH_GOOGLE_ID,
+        clientSecret: AUTH_GOOGLE_SECRET,
+        // checks: ["pkce"], // optional strict enforcement
+      }),
+    ],
+    debug: true,
+  };
+});
